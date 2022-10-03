0d8ef7a90fe2a1914e2a0877c2036f9d7529ddc5
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-10-03 21:36:03 +0100

    added cloudinary .env and file upload path

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const isAuth = require("../middleware/middleware");
+const fileUploader = require("../config/cloudinary.config");
 const Article = require("../models/Article.model");
 
 router.get("/", async (req, res, next) => {
@@ -11,42 +12,48 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/new", isAuth, async (req, res) => {
-  try {
-    const {
-      title,
-      image,
-      city,
-      publisher,
-      other,
-      link,
-      food,
-      lifestyle,
-      guide,
-      review,
-      recipes,
-      publicationDate,
-    } = req.body;
-    const newArticle = await Article.create({
-      title: title,
-      image: image,
-      city: city,
-      publisher: publisher,
-      other: other,
-      link: link,
-      category: {
-        food: food,
-        lifestyle: lifestyle,
-        guide: guide,
-        review: review,
-        recipes: recipes,
-      },
-      publicationDate: publicationDate,
-    });
-    res.status(200).json(newArticle);
-  } catch (error) {
-    res.status(400).send(error.message);
+router.post(
+  "/new",
+  isAuth,
+  fileUploader.single("image"),
+  async (req, res, next) => {
+    try {
+      const {
+        title,
+        image,
+        city,
+        publisher,
+        other,
+        link,
+        food,
+        lifestyle,
+        guide,
+        review,
+        recipes,
+        publicationDate,
+      } = req.body;
+      const newArticle = await Article.create({
+        title: title,
+        image: image,
+        city: city,
+        publisher: publisher,
+        other: other,
+        link: link,
+        category: {
+          food: food,
+          lifestyle: lifestyle,
+          guide: guide,
+          review: review,
+          recipes: recipes,
+        },
+        photo: req.file.path,
+        publicationDate: publicationDate,
+      });
+      res.status(200).json(newArticle);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
   }
-});
+);
 
 module.exports = router;
